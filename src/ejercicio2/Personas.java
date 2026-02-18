900d6abf1f266ee4c0bdff6404542284e8790c85
juanma98117 <juanma98117@alumnos.ilerna.com>
2026-02-18 12:28:22 +0100

    clases creadas

---
+++ src/ejercicio2/Personas.java
@@ -0,0 +1,12 @@
+package ejercicio2;
+
+public class Personas {
+private String nombre;
+private String DNI;
+
+public Personas(String nombre, String DNI) {
+    this.nombre=nombre;
+    this.DNI=DNI;
+
+}
+}
